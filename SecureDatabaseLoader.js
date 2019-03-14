36be8af22d461eb4d0dd9e44dbeb6efd6edc9009
Spencer92 <smhmaslen@gmail.com>
2019-03-13 22:55:59 -0600

    working on loading data

---
+++ SecureDatabaseLoader.js
@@ -0,0 +1,36 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let securedb = new sqlite3.Database(':memory:', (err) => {
+  if(err)
+  {
+    return console.error(err.message);
+  }
+
+  console.log('Connected to secure SQL Database');
+});
+
+
+
+
+securedb.close((err) => {
+  {
+    if(err)
+    {
+      return console.error(err.message);
+    }
+    console.log('Closed secure SQL database');
+  }
+
+});
+
+/*
+async function main() {
+  const port = process.env.PORT || 3000;
+  db = await sqlite.open('./db.sqlite', { cached: true, Promise }).then(db => db.migrate());
+  app.listen(port);
+
+  // This query activates foreign constraints
+  await db.run(SQL`PRAGMA foreign_keys = ON`);
+
+  console.log(`Listening on port ${port}: http://localhost:${port}`);
+}*/
