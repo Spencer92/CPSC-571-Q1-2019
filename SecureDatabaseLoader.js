508b26b43c0e0b8cd56c7a0e3e2add80eaa24ea6
Spencer92 <smhmaslen@gmail.com>
2019-03-16 18:26:15 -0600

    Working on it still

--- SecureDatabaseLoader.js
+++ SecureDatabaseLoader.js
@@ -1,27 +1,157 @@
 const sqlite3 = require('sqlite3').verbose();
-
+const express = require('express');
+const SQL = require('sql-template-strings');
+const bodyParser = require('body-parser');
 
 var decoder = [];
 var position = [];
 var values = [];
 var max;
 
+const app = express();
+/*
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.use(
+    Object.assign(
+      {
+        resave: false,
+        saveUninitialized: true,
+      },
+    ),
+);*/
 
-let securedb = new sqlite3.Database('./securedb.db', (err) => {
+let securedb;
+
+
+
+
+
+app.use(function(req, res, next) {
+  req.securedb = securedb;
+  next();
+});
+
+
+//function main() {
+  const port = process.env.PORT || 3000;
+//  window.alert("Hi");
+  securedb = new sqlite3.Database('./securedb.db', (err) => {
+
+    if(err)
+    {
+      return console.error(err.message);
+    }
+
+/*    securedb.run("CREATE TABLE dummy (data1 INT, data2 TEXT)");
+
+    var statement = ("INSERT INTO dummy VALUES(?,?)");
+
+    //format taken from https://www.w3resource.com/node.js/nodejs-sqlite.php
+    for(var j = 0; j < 10; j++)
+    {
+      var random = Math.random() * 1000;
+      var randomString = Math.random().tostring();
+
+      statement.run(random,randomString);
+    }
+    statement.finalize();
+
+    values.push(req.securedb.get(SQL `SELECT data1, data2 FROM securedb WHERE data1 = (SELECT max(data1) FROM securedb)`));
+    max = req.securedb.get(SQL `SELECT data1, data2 FROM securedb WHERE data1 = (SELECT max(data1) FROM securedb)`);
+    for(var i = 0;
+      max != req.securedb.get(SQL `SELECT data1, data2 FROM securedb WHERE data1 = (SELECT min(data1) FROM securedb)`);
+    i++)
+    {
+      position.push(i);
+      decoder.push(Math.random());
+      max = req.securedb.get(SQL `SELECT data1, data2 FROM securedb WHERE data1 = (SELECT max(data1) FROM securedb WHERE data1 < (SELECT ${req.session.max}))`);
+    }*/
+
+    console.log('Connected to secure SQL Database');
+  });
+
+/*  securedb.run("CREATE TABLE dummy (data1 INT, data2 TEXT)");
+
+  var statement = securedb.prepare("INSERT INTO dummy VALUES(?,?)");
+
+  //format taken from https://www.w3resource.com/node.js/nodejs-sqlite.php
+  for(var j = 0; j < 10; j++)
+  {
+    var random = Math.random() * 1000;
+    var randomString = Math.random().toString();
+
+    statement.run(random,randomString);
+  }
+  statement.finalize();*/
+
+app.get(async (req, res, err) => {
+
   if(err)
   {
     return console.error(err.message);
   }
 
-  values.push(req.securedb.get(SQL `SELECT data1, data2 FROM securedb WHERE data1 = (SELECT max(data1) FROM securedb)`));
-  max = req.securedb.get(SQL `SELECT data1, data2 FROM securedb WHERE data1 = (SELECT max(data1) FROM securedb)`);
+  req.securedb.run("CREATE TABLE dummy (data1 INT, data2 TEXT)");
+
+  var statement = req.securedb.prepare("INSERT INTO dummy VALUES(?,?)");
+
+  //format taken from https://www.w3resource.com/node.js/nodejs-sqlite.php
+  for(var j = 0; j < 10; j++)
+  {
+    var random = Math.random() * 1000;
+    var randomString = Math.random().toString();
+
+    statement.run(random,randomString);
+  }
+  statement.finalize();
+
+  values.push(req.securedb.get(SQL`SELECT data1, data2 FROM securedb WHERE data1 = (SELECT max(data1) FROM securedb)`));
+  max = req.securedb.get(SQL`SELECT data1, data2 FROM securedb WHERE data1 = (SELECT max(data1) FROM securedb)`);
   for(var i = 0;
-    max != req.securedb.get(SQL `SELECT data1, data2 FROM securedb WHERE data1 = (SELECT min(data1) FROM securedb)`);
+    max != req.securedb.get(SQL`SELECT data1, data2 FROM securedb WHERE data1 = (SELECT min(data1) FROM securedb)`);
   i++)
   {
     position.push(i);
     decoder.push(Math.random());
     max = req.securedb.get(SQL `SELECT data1, data2 FROM securedb WHERE data1 = (SELECT max(data1) FROM securedb WHERE data1 < (SELECT ${req.session.max}))`);
+  }
+});
+/*  securedb = await sqlite.open('./db.sqlite', { cached: true, Promise }).then(securedb => securedb.migrate());
+  app.listen(port);
+
+  // This query activates foreign constraints
+  await securedb.run(SQL`PRAGMA foreign_keys = ON`);
+
+  console.log(`Listening on port ${port}: http://localhost:${port}`);*/
+//}
+
+
+
+
+
+
+
+
+
+/*
+let securedb = new sqlite3.Database('./securedb.db', (err) => {
+
+  if(err)
+  {
+    return console.error(err.message);
+  }
+
+  values.push(securedb.get(SQL `SELECT data1, data2 FROM securedb WHERE data1 = (SELECT max(data1) FROM securedb)`));
+  max = securedb.get(SQL `SELECT data1, data2 FROM securedb WHERE data1 = (SELECT max(data1) FROM securedb)`);
+  for(var i = 0;
+    max != securedb.get(SQL `SELECT data1, data2 FROM securedb WHERE data1 = (SELECT min(data1) FROM securedb)`);
+  i++)
+  {
+    position.push(i);
+    decoder.push(Math.random());
+    max = securedb.get(SQL `SELECT data1, data2 FROM securedb WHERE data1 = (SELECT max(data1) FROM securedb WHERE data1 < (SELECT ${session.max}))`);
   }
 
   console.log('Connected to secure SQL Database');
@@ -40,6 +170,7 @@ securedb.close((err) => {
   }
 
 });
+*/
 
 /*
 async function main() {-
