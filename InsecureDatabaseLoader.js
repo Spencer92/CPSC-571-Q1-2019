89f6bed7664e6b1f7013a2b4a13311d6c70be2c8
Spencer Maslen <smhmaslen@gmail.com>
2019-03-22 16:42:22 -0600

    more work done

--- InsecureDatabaseLoader.js
+++ InsecureDatabaseLoader.js
@@ -44,4 +44,39 @@ class ProjectRepository
   Language, CreditCardType, CreditCardNumber, BuyingFrequency]
     )
   }
+
+  update(userData)
+  {
+    const { id, first_name, last_name, Age,
+    gender, Phone, email, City, Username, ip_address,
+  Language, CreditCardType, CreditCardNumber, BuyingFrequency } = userData;
+  return this.theData.run(
+    `UPDATE theData
+    first_name = ?,
+    last_name = ?,
+    Age = ?,
+    gender = ?,
+    Phone = ?,
+    email = ?,
+    City = ?,
+    Username = ?,
+    ip_address = ?,
+    Language = ?,
+    CreditCardType = ?,
+    CreditCardNumber = ?,
+    BuyingFrequency = ?
+    WHERE id = ? `,
+    [ id, first_name, last_name, Age,
+    gender, Phone, email, City, Username, ip_address,
+  Language, CreditCardType, CreditCardNumber, BuyingFrequency ]
+  );
+  }
+
+  delete(id)
+  {
+    return this.theData.Run(
+      `DELETE FROM userData WHERE id = ?`,
+      [id]
+    );
+  }
 }
