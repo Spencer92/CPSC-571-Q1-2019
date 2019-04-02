30dacb549912b99602e23594a8fc0a9b9577e59a
Spencer Maslen <smhmaslen@gmail.com>
2019-04-02 14:38:13 -0600

    added constant values for database loading

--- databaseMaker.js
+++ databaseMaker.js
@@ -2,7 +2,10 @@
 
 const sqlite3 = require('sqlite3').verbose();
 const promise = require('bluebird');
+
+
 var theCounter = 0;
+
 
 
 class InsecureData
--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 const promise = require('bluebird');
 const InsecureData = require('./databaseMaker');
 const DataRepository = require('./InsecureDatabaseLoader');
+const Constants = require('./Constant Values');
+
 
 //from https://stackoverflow.com/questions/6156501/read-a-file-one-line-at-a-time-in-node-js/32599033
 const lineReader = require('readline').createInterface({
